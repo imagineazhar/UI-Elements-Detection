e667216234c223f7a58d3795c26284410d8fafb9
zuzee00 <2muhammadazhar@gmail.com>
2020-02-15 22:22:01 +0500

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import cv2
 import numpy as np
 
@@ -5,72 +7,62 @@ from utils import find_boxes, sharp, sort_contours, get_cord
 
 if __name__ == '__main__':
     # load color image
-    base_address = r'test/'
-    image_file = '6.png'
-    image_path = base_address + image_file
-    original_image = cv2.imread(image_path)
-    dim = (824, 600)
-    original_image = cv2.resize(original_image, dim, interpolation=cv2.INTER_AREA)
-    sharp(original_image)
-    image_color = original_image.copy()
+    base_address = Path('test')
+    image_paths = base_address.glob('*.png')
+    for image_path in image_paths:
+        print(image_path)
+        original_image = cv2.imread(str(image_path))
+        dim = (824, 600)
+        original_image = cv2.resize(original_image, dim, interpolation=cv2.INTER_AREA)
+        sharp(original_image)
+        image_color = original_image.copy()
 
-    # load image
-    img = cv2.imread('sharp.jpg', 0)
-    # A kernel of (3 X 3) ones.
-    kernel = np.ones((3, 3), np.uint8)
+        # load image
+        img = cv2.imread('sharp.jpg', 0)
+        # A kernel of (3 X 3) ones.
+        kernel = np.ones((3, 3), np.uint8)
 
-    # (thresh, img_bin) = cv2.threshold(img, 200, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # (thresh, img_bin) = cv2.threshold(img, 200, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    img_bin = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
+        img_bin = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
 
-    # morphological operation
-    img_bin_eroded = cv2.erode(img_bin, kernel, iterations=1)
+        # morphological operation
+        img_bin_eroded = cv2.erode(img_bin, kernel, iterations=1)
 
-    # Invert Image
-    img_bin = ~img_bin_eroded
+        # Invert Image
+        img_bin = ~img_bin_eroded
 
-    # save image
-    cv2.imwrite("binary.jpg", img_bin)
+        # save image
+        cv2.imwrite("binary.jpg", img_bin)
 
-    img_final_bin = find_boxes(img_bin)
-    cv2.imwrite("img_final_bin.jpg", img_final_bin)
+        img_final_bin = find_boxes(img_bin)
+        cv2.imwrite("img_final_bin.jpg", img_final_bin)
 
-    # Find contours for image, which will detect all the boxes
-    contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Find contours for image, which will detect all the boxes
+        contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Sort all the contours by top to bottom.
-    (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
+        # Sort all the contours by top to bottom.
+        (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
 
-    # Identifying Contours
-    idx = 0
-    cropped_path = 'Cropped/'
-    for c in contours:
+        # Identifying Contours
+        idx = 0
+        cropped_path = 'Cropped/'
+        for c in contours:
 
-        # Returns the location and width,height for every contour
-        x, y, w, h = cv2.boundingRect(c)
+            # Returns the location and width,height for every contour
+            x, y, w, h = cv2.boundingRect(c)
 
-        # get all the pixels coordinates under contour
-        coordinates = []
-        crd = get_cord(x, y, w, h)
-        coordinates.append(crd)
-        #
+            # get all the pixels coordinates under contour
+            coordinates = []
+            crd = get_cord(x, y, w, h)
+            coordinates.append(crd)
+            #
 
-        if 30 > w > 15 > 10 and h < 20:
-            idx += 1
-            # extract ROI
-            new_img = img[y:y + h, x:x + w]
-            cv2.imwrite(cropped_path + str(idx) + '.png', new_img)
+            if 30 > w > 15 > 10 and h < 20:
+                # draw boundary of contour
+                img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
-            # draw boundary of contour
-            img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (255, 255, 0), 2)
-
-        # If the box height is greater then 20, width is >80, then only save it as a box in "cropped/" folder.
-        if 20 < w < 450 and 10 < h < 40:
-            idx += 1
-
-            # ROI extraction
-            new_img = img[y:y + h, x:x + w]
-            cv2.imwrite(cropped_path + str(idx) + '.png', new_img)
-
-            # draw contours
-            img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            # If the box height is greater then 20, width is >80, then only save it as a box in "cropped/" folder.
+            if 20 < w < 450 and 10 < h < 40:
+                # draw contours
+                img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (0, 0, 255), 1)
--- utils.py
+++ utils.py
@@ -11,35 +11,6 @@ def sharp(image):
     cv2.imwrite("sharp.jpg", image_3)
 
 
-# load color image
-base_address = r'test/'
-image_file = '6.png'
-image_path = base_address + image_file
-original_image = cv2.imread(image_path)
-dim = (824, 600)
-original_image = cv2.resize(original_image, dim, interpolation=cv2.INTER_AREA)
-sharp(original_image)
-image_color = original_image.copy()
-
-# load image
-img = cv2.imread('sharp.jpg', 0)
-# A kernel of (3 X 3) ones.
-kernel = np.ones((3, 3), np.uint8)
-
-# (thresh, img_bin) = cv2.threshold(img, 200, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-img_bin = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
-
-# morphological operation
-img_bin_eroded = cv2.erode(img_bin, kernel, iterations=1)
-
-# Invert Image
-img_bin = ~img_bin_eroded
-
-# save image
-cv2.imwrite("binary.jpg", img_bin)
-
-
 def get_cord(x, y, width, height):
     # get all pixels coordinates in a contour
     index = 0
@@ -56,7 +27,7 @@ def get_cord(x, y, width, height):
 def find_boxes(binary_image: np.ndarray):
     # Defining a kernel length
 
-    kernel_length = np.array(img).shape[1] // 195
+    kernel_length = np.array(binary_image).shape[1] // 195
 
     # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
     verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
@@ -87,10 +58,6 @@ def find_boxes(binary_image: np.ndarray):
     return cv2.adaptiveThreshold(~img_final_bin, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
 
 
-img_final_bin = find_boxes(img_bin)
-cv2.imwrite("img_final_bin.jpg", img_final_bin)
-
-
 def sort_contours(cnts, method="top-to-bottom"):
     # initialize the reverse flag and sort index
     reverse = False
@@ -105,95 +72,9 @@ def sort_contours(cnts, method="top-to-bottom"):
         i = 1
 
     # construct the list of bounding boxes and sort them from top to bottom
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
+    bounding_boxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, bounding_boxes) = zip(*sorted(zip(cnts, bounding_boxes),
+                                         key=lambda b: b[1][i], reverse=reverse))
 
     # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
-
-
-# Find contours for image, which will detect all the boxes
-contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# Sort all the contours by top to bottom.
-(contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
-
-# Identifying Contours
-idx = 0
-for c in contours:
-
-    # Returns the location and width,height for every contour
-    x, y, w, h = cv2.boundingRect(c)
-
-    # get all the pixels coordinates under contour
-    coordinates = []
-    crd = get_cord(x, y, w, h)
-    coordinates.append(crd)
-    #
-
-    if 30 > w > 15 > 10 and h < 20:
-        idx += 1
-
-        # draw boundary of contour
-        img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (255, 255, 0), 2)
-
-    # If the box height is greater then 20, width is >80, then only save it as a box in "cropped/" folder.
-    if (w > 20 and h > 10) and w < 450 and h < 40:
-        idx += 1
-
-        # ROI extraction
-
-        # draw contours
-        img_outlined = cv2.rectangle(image_color, (x, y), (x + w, y + h), (0, 0, 255), 1)
-
-# 1. gets pixels of contours
-# 2. gets coordinate of right-click 
-# 3. compare coordinates (click) with pixel coordinate from contour
-# 4. if they match, draw boundary of that contour
-
-drawing = False  # true if mouse is pressed
-mode = True  # if True, draw rectangle.
-
-
-def mouse_func(event, x, y, flags, param, drawing: bool = False, mode: bool = True):
-    if event == cv2.EVENT_MOUSEMOVE:
-        drawing = True
-    # right click co-ordinates
-    cord = [x, y]
-    for c in contours:
-
-        # staring points (x,y) and dimensions of contour (w=width, h= height)
-        x, y, w, h = cv2.boundingRect(c)
-        if 15 < w < 30 > 15 and h < 30:
-            cor = get_cord(x, y, w, h)
-            for i in cor:
-                if i == cord and drawing and mode:
-                    cv2.rectangle(original_image, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        if (w > 20 and h > 10) and w < 445 and h < 45:
-            cor = get_cord(x, y, w, h)
-            for i in cor:
-                if i == cord and drawing and mode:
-                    cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 0, 255), 1)
-
-
-cv2.namedWindow('Picture', 1)
-cv2.setMouseCallback("Picture", mouse_func)
-while 1:
-    cv2.imshow('Picture', original_image)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-
-    # if pressed "p" exit the pop-up window
-    elif k == ord('p'):
-        break
-cv2.destroyAllWindows()
-
-# Show all contours
-for c in contours:
-    # Returns the location and width,height for every contour
-    x, y, w, h = cv2.boundingRect(c)
-
-    # if w > 1.3*h and 40>h>10 and w<450:
-    clk = cv2.rectangle(image_color, (x, y), (x + w, y + h), (0, 150, 100), 2)
+    return cnts, bounding_boxes
